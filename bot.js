6ca989ea5ad903458e9c82e732711f6bd88385a2
Jack Jarvis <jackpjarvis12@gmail.com>
2019-07-20 11:28:11 +0100

    Add basic draft

--- bot.js
+++ bot.js
@@ -2,8 +2,19 @@ var Discord = require('discord.js')
 var client = new Discord.Client()
 var auth = require('./auth.json')
 var fs = require('fs')
+var shuffle = require('shuffle-array')
 
 var civs = JSON.parse(fs.readFileSync("civs.json")).civs
+
+function getDraft(numberOfPlayers, civsPerPlayer) {
+    shuffle(civs)
+    draft = []
+    for (i=0; i<numberOfPlayers; i++)
+    {
+        draft.push(civs.slice(i*civsPerPlayer, (i+1)*civsPerPlayer));
+    }
+    return draft
+}
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
@@ -11,7 +22,19 @@ client.on('ready', () => {
 
 client.on('message', msg => {
     if (msg.content === '!draft') {
-        msg.channel.send(civs[0])
+        draft = getDraft(5,3)
+        console.log(draft)
+        message = ""
+        for (i=0; i<draft.length; i++)
+        {
+            message += `Player ${i} - `
+            for (j=0; j<draft[i].length-1; j++)
+            {
+                message += `${draft[i][j]} / `
+            }
+            message += `${draft[i][draft[i].length-1]}\n`
+        }
+        msg.channel.send(message)
     }
 })
 
