156edcece4ce8ec61ea24eec6a0470936716fa1d
Jack Jarvis <jackpjarvis12@gmail.com>
2021-05-15 16:40:54 +0100

    Extract messages

--- bot/bot.ts
+++ bot/bot.ts
@@ -1,11 +1,10 @@
-import { DMChannel, Message, NewsChannel, TextChannel, VoiceChannel } from "discord.js"
-import { Client, Discord, On } from "@typeit/discord"
-import {draft, Draft, PlayerDraft} from "./draft.js"
+import { Message, VoiceChannel } from "discord.js"
+import { Client } from "@typeit/discord"
+import {draft, PlayerDraft} from "./draft.js"
+import Messages from "./messages.js"
 
 var client = new Client()
 var auth = require('./auth.json')
-
-type DiscordTextChannel = TextChannel | DMChannel | NewsChannel
 
 function getPlayerDraftString(playerName: string, playerDraft: PlayerDraft) : string
 {
@@ -34,35 +33,6 @@ function extractArgValue(args: Array<string>, argName: string) : number | undefi
     }
 }
 
-function sendBadlyFormedError(channel: DiscordTextChannel) 
-{
-    channel.send("Command is badly formed - see `civbot help` for guidance")
-}
-
-function sendDraftFailedError(channel: DiscordTextChannel) 
-{
-    channel.send("Draft failed - no players found. Either join voice or set some AI.")
-}
-
-function sendHelpMessage(channel: DiscordTextChannel) 
-{
-    channel.send(
-        `\`\`\`civbot draft [OPTIONS]
-        Drafts 3 civs for each player in the voice channel. Additional options:
-            - civs [NUMBER] : Change the number of civs for each player
-            - ai [NUMBER] : Add a specified number of AI players
-            - novoice : Don't include players from voice
-            - lekmod : Include lekmod civs
-            - lekmod-only : Only use lekmod civs (no vanilla)
-        eg. civbot draft numcivs 5 ai 2
-        Drafts a game with everyone in voice plus 2 AI, and everyone picks from 5 civs.\`\`\``)
-}
-
-function sendInfoMessage(channel: DiscordTextChannel)
-{
-    channel.send("Hi, I'm CivBot. Use me to draft civ games with `civbot draft`, it's all I'm good for. \nTo find out more about drafting games, try `civbot help`.")
-}
-
 client.on('ready', () => 
 {
     console.log(`Logged in as ${client.user!.tag}!`)
@@ -74,12 +44,12 @@ client.on('message', (msg: Message) =>
     {
         let args = msg.content.split(" ")
 
-        msg.channel.send("Wowser!")
+        msg.channel.send(Messages.Wowser)
 
         // civbot
         if (args.length == 1) 
         {
-            sendInfoMessage(msg.channel)
+            msg.channel.send(Messages.Info)
         }
 
         // civbot draft
@@ -95,7 +65,7 @@ client.on('message', (msg: Message) =>
                 let ai = extractArgValue(args, "ai")
                 if (ai === undefined) 
                 {
-                    sendBadlyFormedError(msg.channel)
+                    msg.channel.send(Messages.BadlyFormed)
                     return
                 }
             }
@@ -105,7 +75,7 @@ client.on('message', (msg: Message) =>
                 let numCivs = extractArgValue(args, "civs")
                 if (numCivs === undefined) 
                 {
-                    sendBadlyFormedError(msg.channel)
+                    msg.channel.send(Messages.BadlyFormed)
                     return
                 }
             }
@@ -117,7 +87,7 @@ client.on('message', (msg: Message) =>
             let voiceChannel = client.channels.cache.get(msg.member!.voice.channelID!) as VoiceChannel | undefined
             if (voiceChannel === undefined)
             {
-                msg.channel.send("You're not in voice, so I'll set `novoice` on for this draft.")
+                msg.channel.send(Messages.NotInVoice)
                 useVoice = false
             }
 
@@ -143,7 +113,7 @@ client.on('message', (msg: Message) =>
 
             if (response === "")
             {
-                sendDraftFailedError(msg.channel)
+                msg.channel.send(Messages.DraftFailed)
             }
             else
             {
@@ -154,7 +124,7 @@ client.on('message', (msg: Message) =>
         // civbot help
         if (args[1] === 'help')
         {
-            sendHelpMessage(msg.channel)
+            msg.channel.send(Messages.Help)
         }
     }
 })
---
+++ bot/messages.ts
@@ -0,0 +1,18 @@
+export default class Messages {
+    public static get Help() : string { 
+        return `\`\`\`civbot draft [OPTIONS]
+        Drafts 3 civs for each player in the voice channel. Additional options:
+            - civs [NUMBER] : Change the number of civs for each player
+            - ai [NUMBER] : Add a specified number of AI players
+            - novoice : Don't include players from voice
+            - lekmod : Include lekmod civs
+            - lekmod-only : Only use lekmod civs (no vanilla)
+        eg. civbot draft numcivs 5 ai 2
+        Drafts a game with everyone in voice plus 2 AI, and everyone picks from 5 civs.\`\`\`` 
+    }
+    public static get Info() : string { return "Hi, I'm CivBot. Use me to draft civ games with `civbot draft`, it's all I'm good for. \nTo find out more about drafting games, try `civbot help`." }
+    public static get DraftFailed() : string { return "Draft failed - no players found. Either join voice or set some AI." }
+    public static get BadlyFormed() : string { return "Command is badly formed - see `civbot help` for guidance" }
+    public static get NotInVoice() : string { return "You're not in voice, so I'll set `novoice` on for this draft." }
+    public static get Wowser() : string { return "Wowser!" }
+}
