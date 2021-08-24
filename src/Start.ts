e38556a501bb58254b435213d0665e922a1f851c
Jack Jarvis <jackpjarvis12@gmail.com>
2021-08-24 18:12:24 +0100

    Don't crash the bot on error

--- src/Start.ts
+++ src/Start.ts
@@ -1,6 +1,7 @@
 import { Client, Intents } from "discord.js"
 import { handleMessage } from "./HandleMessage"
 import { handleSlashCommand } from "./HandleSlashCommand"
+import Messages from "./Messages"
 var auth = require('../auth/auth.json')
 
 async function start() {
@@ -18,11 +19,23 @@ async function start() {
   client.on("interactionCreate", async (interaction) => {
     if (!interaction.isCommand()) return
 
-    await handleSlashCommand(interaction)
+    try {
+      await handleSlashCommand(interaction)
+    }
+    catch (e) {
+      console.log(e)
+      interaction.reply(Messages.GenericError)
+    }
   })
 
   client.on("messageCreate", async (message) => {
-    await handleMessage(message, client)
+    try {
+      await handleMessage(message, client)
+    }
+    catch (e) {
+      console.log(e)
+      message.channel.send(Messages.GenericError)
+    }
   })
 
   await client.login(auth.token)
