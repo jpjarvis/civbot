6d991183d6fb1861db4938376755c15598ab3f57
Jack Jarvis <jackpjarvis12@gmail.com>
2021-09-20 18:36:04 +0100

    Use ts-node

--- src/HandleMessage.ts
+++ src/HandleMessage.ts
@@ -1,10 +1,10 @@
-import Messages from "./Messages.js"
-import UserData from "./UserData.js"
-import { CivGroup } from "./CivGroups.js"
-import { getVoiceChannel } from "./DiscordUtils.js"
-import { DraftArguments, draftCommand } from "./DraftCommand.js"
+import Messages from "./Messages"
+import UserData from "./UserData"
+import { CivGroup } from "./CivGroups"
+import { getVoiceChannel } from "./DiscordUtils"
+import { DraftArguments, draftCommand } from "./DraftCommand"
 import { Client, Message } from "discord.js"
-import { draft } from "./Draft.js"
+import { draft } from "./Draft"
 
 function extractArgValue(args: Array<string>, argName: string): number | undefined {
     let index = args.findIndex((a) => a === argName)
