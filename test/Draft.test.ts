c55cf2a3d591675acb806b4c0debaae55889b420
Jack Jarvis <jackpjarvis12@gmail.com>
2021-11-19 15:56:44 +0000

    Add basic draft test

---
+++ test/Draft.test.ts
@@ -0,0 +1,10 @@
+﻿import {draft} from "../src/Draft";
+
+test('draft should draft the correct number of civs for each player', () => {
+    let draftResult = draft(3, 3, ['civ1','civ2','civ3','civ4','civ5','civ6','civ7','civ8','civ9'])
+    expect(draftResult.length).toBe(3)
+    
+    draftResult.forEach(playerDraft => {
+        expect(playerDraft.length).toBe(3)
+    })
+})
