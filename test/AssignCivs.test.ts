a3647e2e3f45e691c87720d48d04d532707e5cff
Jack Jarvis <jackpjarvis12@gmail.com>
2021-11-26 18:02:15 +0000

    Add tests for assignCivs

---
+++ test/AssignCivs.test.ts
@@ -0,0 +1,17 @@
+﻿import {assignCivs} from "../src/AssignCivs";
+
+function generateArray(count: number): string[] {
+    return Array.from(Array(count)).map((_,i) => `item${i}`)
+}
+
+describe('assignCivs', () => {
+    test('should draft the right number of civs for all players',() => {
+        const players = generateArray(3)
+        const draft = assignCivs(players, 3, generateArray(50));
+        
+        for (const player of players) {
+            console.log(player)
+            expect(draft[player]).toHaveLength(3)
+        }
+    })
+})
