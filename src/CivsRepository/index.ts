1d1124c23903299e33f3c3c68482427fe73b7ce1
Jack Jarvis <jackpjarvis12@gmail.com>
2021-12-10 18:17:47 +0000

    Remove unused singletons

--- src/CivsRepository/index.ts
+++
@@ -1,4 +0,0 @@
-import {CivsRepository} from "./interface";
-import FileAndUserDataCivsRepository from "./FileAndUserDataCivsRepository";
-
-export const CivsRepositoryInstance : CivsRepository = new FileAndUserDataCivsRepository()
--- src/UserDataStore/index.ts
+++
@@ -1,5 +0,0 @@
-import FileUserDataStore from "./FileUserDataStore";
-import {UserDataStore} from "./interface";
-import PostgresDataStore from "./PostgresDataStore";
-
-export const UserDataStoreInstance : UserDataStore = process.env['DATABASE_URL'] ? new PostgresDataStore() : new FileUserDataStore()
