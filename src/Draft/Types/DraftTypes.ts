d7ed5e27bbd39f421d0542c11a6e62d75c95031b
Jack Jarvis <jackpjarvis12@gmail.com>
2022-03-13 01:36:21 +0000

    Remove unused DraftResult

--- src/Draft/Types/DraftTypes.ts
+++ src/Draft/Types/DraftTypes.ts
@@ -1,4 +1,3 @@
 ﻿export type Draft = Map<string, string[]>
 
 export type DraftError = "no-players" | "not-enough-civs"
-export type DraftResult = {success: true, draft: Draft} | {success: false, error: DraftError}
