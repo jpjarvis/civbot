18418020266d8134c4f02ea462ba88df28e2f67a
Jack Jarvis <jackpjarvis12@gmail.com>
2022-03-18 18:03:09 +0000

    Add tests for default draft arguments

--- __tests__/Commands/Draft/DraftCommand.test.ts
+++ __tests__/Commands/Draft/DraftCommand.test.ts
@@ -2,10 +2,11 @@
 import UserData from "../../../src/CivBot/Types/UserData";
 import {CivData} from "../../../src/CivBot/CivData";
 import {extractErrorAndAssertIsError, extractResultAndAssertIsNotError, generateArray} from "../../TestUtils";
+import {CivGroup} from "../../../src/CivBot/Types/CivGroups";
 
 describe('draftCommand', () => {
     
-    const userData: UserData = {
+    const emptyUserData: UserData = {
         defaultDraftSettings: {},
         customCivs: []
     }
@@ -29,7 +30,7 @@ describe('draftCommand', () => {
             civGroups: ['civ5-vanilla']
         }
         
-        const result = await draftCommand(draftArgs, [], userData, civData);
+        const result = await draftCommand(draftArgs, [], emptyUserData, civData);
         expect(result.draftResult.isError).toBe(true)
         if (!result.draftResult.isError) {
             return;
@@ -45,9 +46,37 @@ describe('draftCommand', () => {
             civGroups: ['civ5-vanilla']
         }
 
-        const result = await draftCommand(draftArgs, ["player1"], userData, civData);
+        const result = await draftCommand(draftArgs, ["player1"], emptyUserData, civData);
         const error = extractErrorAndAssertIsError(result.draftResult);
 
         expect(error).toBe("not-enough-civs");
+    });
+    
+    it('should fall back to default arguments if none are provided in arguments or user data', async () => {
+        const result = await draftCommand({}, ["player1"], emptyUserData, civData);
+        
+        const draft = extractResultAndAssertIsNotError(result.draftResult);
+        
+        expect(draft).toHaveLength(1);
+        expect(draft[0].player).toBe("player1");
+        expect(result.civGroupsUsed).toStrictEqual<CivGroup[]>(["civ5-vanilla"]);
+    });
+    
+    it('should fall back to user data arguments if none provided in arguments', async () => {
+        const userData : UserData = {
+            defaultDraftSettings: {
+                numberOfAi: 1,
+                civGroups: ["civ6-vanilla"],
+                numberOfCivs: 5
+            },
+            customCivs: []
+        }
+        
+        const result = await draftCommand({}, ["player1"], userData, civData); 
+        const draft = extractResultAndAssertIsNotError(result.draftResult);
+        
+        expect(draft).toHaveLength(2);
+        expect(draft[0].civs).toHaveLength(5);
+        expect(result.civGroupsUsed).toStrictEqual(["civ6-vanilla"]);
     });
 })
