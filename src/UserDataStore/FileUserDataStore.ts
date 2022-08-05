e4815d3fb2712e51fcdcc9e0b51638e07e06004e
jack <jackpjarvis12@gmail.com>
2022-08-05 20:04:31 +0100

    Use async in FileUserDataStore

--- src/UserDataStore/FileUserDataStore.ts
+++ src/UserDataStore/FileUserDataStore.ts
@@ -8,28 +8,25 @@ function getUserDataPath(tenantId: string) {
 }
 
 async function ensureExists(filePath: string): Promise<void> {
-    return fs.promises
-        .mkdir(path.dirname(filePath), { recursive: true })
-        .then(() => fs.promises.readFile(filePath))
-        .then(() => {
-            return;
-        })
-        .catch(() => {
-            fs.promises.writeFile(filePath, JSON.stringify(createEmptyUserData()));
-            console.log(`Creating ${filePath}`);
-        });
+    await fs.promises.mkdir(path.dirname(filePath), { recursive: true });
+    try {
+        await fs.promises.readFile(filePath);
+    } catch {
+        await fs.promises.writeFile(filePath, JSON.stringify(createEmptyUserData()));
+        console.log(`Creating ${filePath}`);
+    }
 }
 
 export const FileUserDataStore: UserDataStore = {
     save: async (tenantId: string, userData: UserData) => {
         let filePath = getUserDataPath(tenantId);
-        return ensureExists(filePath).then(() => fs.promises.writeFile(filePath, JSON.stringify(userData)));
+        await ensureExists(filePath);
+        await fs.promises.writeFile(filePath, JSON.stringify(userData));
     },
 
     load: async (tenantId: string) => {
         let filePath = getUserDataPath(tenantId);
-        return ensureExists(filePath)
-            .then(() => fs.promises.readFile(filePath, "utf8"))
-            .then((value) => JSON.parse(value));
+        await ensureExists(filePath);
+        return JSON.parse(await fs.promises.readFile(filePath, "utf8"));
     },
 };
