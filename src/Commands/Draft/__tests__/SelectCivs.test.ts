f8f002a49e9e61b5c3e6d548f07b17a45525300b
jack <jackpjarvis12@gmail.com>
2022-09-10 11:20:18 +0100

    Test for banned civs

--- src/Commands/Draft/__tests__/SelectCivs.test.ts
+++ src/Commands/Draft/__tests__/SelectCivs.test.ts
@@ -6,27 +6,32 @@ const customCivs = ["custom1"];
 
 describe("selectCivs", () => {
     it("should return no civs if no civ groups are given", () => {
-        const civs = selectCivs(new Set<CivGroup>(), customCivs);
+        const civs = selectCivs(new Set<CivGroup>(), customCivs, []);
         expect(civs).toHaveLength(0);
     });
 
     it("should return the civs of one civ group when specified", () => {
-        const civs = selectCivs(new Set(["civ5-vanilla"]), customCivs);
+        const civs = selectCivs(new Set(["civ5-vanilla"]), customCivs, []);
         expect(civs).toIncludeSameMembers(Civs["civ5-vanilla"]);
     });
 
     it("should include custom civs from user data if specified", () => {
-        const civs = selectCivs(new Set(["custom"]), customCivs);
+        const civs = selectCivs(new Set(["custom"]), customCivs, []);
         expect(civs).toIncludeSameMembers(["custom1"]);
     });
 
     it("should return the civs of multiple groups when specified", () => {
-        const civs = selectCivs(new Set(["civ5-vanilla", "lekmod"]), customCivs);
+        const civs = selectCivs(new Set(["civ5-vanilla", "lekmod"]), customCivs, []);
         expect(civs).toIncludeSameMembers(Civs["civ5-vanilla"].concat(Civs["lekmod"]));
     });
 
     it("should include custom civs with normal groups when specified", () => {
-        const civs = selectCivs(new Set(["civ5-vanilla", "lekmod", "custom"]), customCivs);
+        const civs = selectCivs(new Set(["civ5-vanilla", "lekmod", "custom"]), customCivs, []);
         expect(civs).toIncludeSameMembers(Civs["civ5-vanilla"].concat(Civs["lekmod"]).concat(customCivs));
+    });
+
+    it("should not include banned civs", () => {
+        const civs = selectCivs(new Set(["civ5-vanilla"]), [], ["Russia"]);
+        expect(civs).not.toInclude("Russia");
     });
 });
