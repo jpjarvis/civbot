d244179822ce496870362205a2cdba70fcaf8459
Jack Jarvis <jackpjarvis12@gmail.com>
2023-03-18 20:30:29 +0000

    Sort the custom civs from config

--- src/Commands/Config/ShowConfigCommand.ts
+++ src/Commands/Config/ShowConfigCommand.ts
@@ -4,7 +4,7 @@ export function showConfigCommand(userSettings: UserSettings): string {
     let response = "";
     response += `Using civ groups: \`\`\`\n${userSettings.defaultDraftSettings?.civGroups?.join("\n")}\`\`\`` + "\n";
     if (userSettings.customCivs) {
-        response += `Custom civs:\`\`\`\n${userSettings.customCivs.join("\n")}\`\`\``;
+        response += `Custom civs:\`\`\`\n${userSettings.customCivs.sort().join("\n")}\`\`\``;
     }
     return response;
 }
