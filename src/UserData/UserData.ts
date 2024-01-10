0aa8a5488770b98388173c419470abe1b531920e
Jack Jarvis <jackpjarvis12@gmail.com>
2024-01-10 13:40:01 +0000

    Add default profiles

--- src/UserData/UserData.ts
+++ src/UserData/UserData.ts
@@ -16,6 +16,28 @@ export function createDefaultUserData(): UserData {
             customCivs: [],
             bannedCivs: []
         },
-        profiles: {},
+        profiles: {
+            "civ5": {
+                defaultDraftSettings: {
+                    civGroups: ["civ5-vanilla"]
+                },
+                customCivs: [],
+                bannedCivs: []
+            },
+            "civ5-lekmod": {
+                defaultDraftSettings: {
+                    civGroups: ["civ5-vanilla", "lekmod"]
+                },
+                customCivs: [],
+                bannedCivs: []
+            },
+            "civ6": {
+                defaultDraftSettings: {
+                    civGroups: ["civ6-vanilla", "civ6-rnf", "civ6-gs", "civ6-extra", "civ6-frontier", "civ6-leaderpass", "civ6-personas"]
+                },
+                customCivs: [],
+                bannedCivs: []
+            }
+        },
     };
 }
