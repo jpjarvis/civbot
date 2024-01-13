a020a1267948fdc75093c26a467a5f7f9096fa32
Jack Jarvis <jackpjarvis12@gmail.com>
2024-01-13 17:13:22 +0000

    Simplify Messages

--- src/Messages.ts
+++ src/Messages.ts
@@ -1,17 +1,8 @@
-export default class Messages {
-    public static get NoPlayers(): string {
-        return "Draft failed - no players found. Either join voice or set some AI.";
-    }
-
-    public static get NotEnoughCivs(): string {
-        return "Draft failed - not enough civs to assign to the players. Either add more civ groups to your draft or decrease the number of civs per player.";
-    }
-
-    public static get GenericError(): string {
-        return "Uh oh, something's gone wrong. It's probably Jack's fault.";
-    }
-
-    public static get Wowser(): string {
-        return "Wowser!";
-    }
+const Messages = {
+    NoPlayers: "Draft failed - no players found. Either join voice or set some AI.",
+    NotEnoughCivs: "Draft failed - not enough civs to assign to the players. Either add more civ groups to your draft or decrease the number of civs per player.",
+    GenericError: "Uh oh, something's gone wrong. It's probably Jack's fault.",
+    Wowser: "Wowser!"
 }
+
+export default Messages;
