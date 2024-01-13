1f302c7d9b9717fa381579a9c9d696cae0156027
Jack Jarvis <jackpjarvis12@gmail.com>
2024-01-13 18:08:37 +0000

    Tidy up civ group metadata

--- src/Civs/CivGroups.ts
+++ src/Civs/CivGroups.ts
@@ -5,22 +5,18 @@ export type CivGroup = keyof typeof Civs | "custom";
 export const CivGroups = Object.keys(Civs).concat("custom") as CivGroup[];
 
 const CivGroupMetadata: { [a in CivGroup]: { displayName: string; game: CivGame | "All" } } = {
-    "civ6-extra": {displayName: "Standalone DLC Civs", game: "Civ 6"},
-    "civ6-frontier": {displayName: "New Frontier Pass", game: "Civ 6"},
-    "civ6-gs": {displayName: "Gathering Storm", game: "Civ 6"},
-    "civ6-leaderpass": {displayName: "Leader Pass", game: "Civ 6"},
-    "civ6-personas": {displayName: "Persona packs", game: "Civ 6"},
+    "civ5-vanilla": {displayName: "Base game + DLC", game: "Civ 5"},
+    "lekmod": {displayName: "LEKMOD", game: "Civ 5"},
+
+    "civ6-vanilla": {displayName: "Base game", game: "Civ 6"},
     "civ6-rnf": {displayName: "Rise & Fall", game: "Civ 6"},
-    custom: {displayName: "Custom civs", game: "All"},
-    lekmod: {displayName: "LEKMOD", game: "Civ 5"},
-    "civ5-vanilla": {
-        displayName: "Base game + DLC",
-        game: "Civ 5"
-    },
-    "civ6-vanilla": {
-        displayName: "Base game",
-        game: "Civ 6"
-    }
+    "civ6-gs": {displayName: "Gathering Storm", game: "Civ 6"},
+    "civ6-frontier": {displayName: "New Frontier Pass", game: "Civ 6"},
+    "civ6-leaderpass": {displayName: "Leader Pass", game: "Civ 6"},
+    "civ6-extra": {displayName: "Standalone DLC Civs", game: "Civ 6"},
+    "civ6-personas": {displayName: "Persona packs", game: "Civ 6"},
+
+    "custom": {displayName: "Custom civs", game: "All"},
 }
 
 export function civGroupsInGame(game: CivGame) {
