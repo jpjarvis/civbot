0207f024e1efc8d25f360a25547d25c401eb1cb6
Jack Jarvis <jackpjarvis12@gmail.com>
2024-02-12 18:31:37 +0000

    extract methods

--- src/Commands/Ban/BanCommand.ts
+++ src/Commands/Ban/BanCommand.ts
@@ -1,7 +1,7 @@
 import {loadUserData, saveUserData} from "../../UserDataStore";
 import {matchCivs} from "./CivExists";
 import {Civ, hasLeader} from "../../Civs/Civs";
-import {ChatInputCommandInteraction} from "discord.js";
+import {ChatInputCommandInteraction, Message} from "discord.js";
 import {UserData} from "../../UserData/UserData";
 import {logInfo} from "../../Log";
 
@@ -16,39 +16,29 @@ export async function handleBan(interaction: ChatInputCommandInteraction) {
     if (matchedCivs.length === 0) {
         await interaction.reply(`No civ called ${civToBan} exists in ${userData.game}.`);
         return;
-    }
-    else if (matchedCivs.length > numberEmojis.length) {
+    } else if (matchedCivs.length > numberEmojis.length) {
         await interaction.reply(`"${civToBan}" matches ${matchedCivs.length} civs. Please me more specific.`);
-    }
-    else if (matchedCivs.length > 1) {
+        return;
+    } else if (matchedCivs.length > 1) {
         const message = await interaction.reply({
             content: renderCivSelectionMessage(civToBan, matchedCivs),
             fetchReply: true
         });
 
-        await message.react(allEmoji)
-        for (const emoji of numberEmojis.slice(0, matchedCivs.length)) {
-            await message.react(emoji);
-        }
-
         logInfo("Matched multiple civs. Waiting for user reaction...");
-        const userReactions = await message.awaitReactions({
-            filter: (reaction, user) => numberEmojis.concat(allEmoji).includes(reaction.emoji.name ?? "") && user.id === interaction.user.id,
-            time: 60_000,
-            max: 1
-        });
+        await addReactions(message, matchedCivs.length);
+        const reactionEmoji = await waitForUserReaction(message, interaction.user.id);
 
-        const reaction = userReactions.first();
-        if (reaction === undefined) {
+        if (reactionEmoji === undefined) {
             await message.edit(`Request to ban civ timed out.`);
             return;
         }
-        if (reaction.emoji.name === allEmoji) {
+        if (reactionEmoji === allEmoji) {
             await banCivs(serverId, userData, matchedCivs);
             await message.edit(banMessage(matchedCivs));
             return;
-        } else if (numberEmojis.includes(reaction.emoji.name!)) {
-            const chosenCiv = matchedCivs[numberEmojis.indexOf(reaction.emoji.name!)];
+        } else if (numberEmojis.includes(reactionEmoji)) {
+            const chosenCiv = matchedCivs[numberEmojis.indexOf(reactionEmoji)];
             await banCivs(serverId, userData, [chosenCiv]);
             await message.edit(banMessage([chosenCiv]));
             return;
@@ -56,9 +46,7 @@ export async function handleBan(interaction: ChatInputCommandInteraction) {
     }
 
     await banCivs(serverId, userData, matchedCivs);
-    const message = banMessage(matchedCivs);
-
-    await interaction.reply(message);
+    await interaction.reply(banMessage(matchedCivs));
 }
 
 async function banCivs(serverId: string, userData: UserData, civsToBan: Civ[]) {
@@ -70,8 +58,25 @@ function banMessage(bannedCivs: Civ[]) {
     if (bannedCivs.length == 1) {
         return `\`${renderCiv(bannedCivs[0])}\` has been banned. It will no longer appear in your drafts.`;
     }
-    
+
     return `${bannedCivs.map(x => `\`${x}\``).join(", ")} have been banned. They will no longer appear in your drafts.`;
+}
+
+async function addReactions(message: Message, numChoices: number) {
+    await message.react(allEmoji)
+    for (const emoji of numberEmojis.slice(0, numChoices)) {
+        await message.react(emoji);
+    }
+}
+
+async function waitForUserReaction(message: Message, userId: string) {
+    const userReactions = await message.awaitReactions({
+        filter: (reaction, user) => numberEmojis.concat(allEmoji).includes(reaction.emoji.name ?? "") && user.id === userId,
+        time: 60_000,
+        max: 1
+    });
+
+    return userReactions.first()?.emoji?.name ?? undefined;
 }
 
 function renderCivSelectionMessage(civToBan: Civ, matchedCivs: Civ[]) {
