5597d030df3325daa28c06fbec8e296c81eba636
Jack Jarvis <jackpjarvis12@gmail.com>
2024-02-13 19:09:49 +0000

    Pull out multiple choice as its own thing

--- src/Commands/Ban/BanCommand.ts
+++ src/Commands/Ban/BanCommand.ts
@@ -1,9 +1,9 @@
 import {loadUserData, saveUserData} from "../../UserDataStore";
 import {matchCivs} from "./CivExists";
 import {Civ, civsEqual, hasLeader} from "../../Civs/Civs";
-import {ChatInputCommandInteraction, Message} from "discord.js";
+import {ChatInputCommandInteraction} from "discord.js";
 import {UserData} from "../../UserData/UserData";
-import {logInfo} from "../../Log";
+import {MAX_OPTIONS, multipleChoice} from "./MultipleChoice";
 
 export async function handleBan(interaction: ChatInputCommandInteraction) {
     const serverId = interaction.guildId!;
@@ -14,42 +14,27 @@ export async function handleBan(interaction: ChatInputCommandInteraction) {
     const allMatchedCivs = matchCivs(searchString, userData.game);
     const alreadyBannedCivs = allMatchedCivs.filter(x => isBanned(x, userData))
     const availableMatchedCivs = allMatchedCivs.filter(x => !isBanned(x, userData))
-    
+
     if (availableMatchedCivs.length === 0) {
         if (alreadyBannedCivs.length == 1) {
             await interaction.reply(`\`${renderCiv(alreadyBannedCivs[0])}\` is already banned.`);
-        }
-        else if (alreadyBannedCivs.length > 1) {
+        } else if (alreadyBannedCivs.length > 1) {
             await interaction.reply(`"${searchString}" matches ${alreadyBannedCivs.length} civs, but they are all already banned.`);
         }
         await interaction.reply(`No civ called "${searchString}" exists in ${userData.game}.`);
         return;
-    } else if (availableMatchedCivs.length > numberEmojis.length) {
+    } else if (availableMatchedCivs.length > MAX_OPTIONS) {
         await interaction.reply(`"${searchString}" matches ${availableMatchedCivs.length} civs that aren't already banned. Please me more specific.`);
         return;
     } else if (availableMatchedCivs.length > 1) {
-        const message = await interaction.reply({
-            content: renderCivSelectionMessage(searchString, availableMatchedCivs),
-            fetchReply: true
+        const chosenCivs = await multipleChoice(interaction, availableMatchedCivs.map(renderCiv), {
+            question: `"${searchString}" matches ${availableMatchedCivs.length} civs that aren't already banned. Please click one of the reacts to select which one to ban:`,
+            selected: banMessage,
+            timeout: "Request to ban civ timed out."
         });
 
-        logInfo("Matched multiple civs. Waiting for user reaction...");
-        await addReactions(message, availableMatchedCivs.length);
-        const reactionEmoji = await waitForUserReaction(message, interaction.user.id);
-
-        if (reactionEmoji === undefined) {
-            await message.edit(`Request to ban civ timed out.`);
-            return;
-        }
-        if (reactionEmoji === allEmoji) {
-            await banCivs(serverId, userData, availableMatchedCivs);
-            await message.edit(banMessage(availableMatchedCivs));
-            return;
-        } else if (numberEmojis.includes(reactionEmoji)) {
-            const chosenCiv = availableMatchedCivs[numberEmojis.indexOf(reactionEmoji)];
-            await banCivs(serverId, userData, [chosenCiv]);
-            await message.edit(banMessage([chosenCiv]));
-            return;
+        if (chosenCivs) {
+            await banCivs(serverId, userData, chosenCivs)
         }
     }
 
@@ -74,29 +59,6 @@ function banMessage(bannedCivs: Civ[]) {
     return `The following civs have been banned: \`\`\`${bannedCivs.map(x => renderCiv(x)).join("\n")}\`\`\` They will no longer appear in your drafts.`;
 }
 
-async function addReactions(message: Message, numChoices: number) {
-    await message.react(allEmoji)
-    for (const emoji of numberEmojis.slice(0, numChoices)) {
-        await message.react(emoji);
-    }
-}
-
-async function waitForUserReaction(message: Message, userId: string) {
-    const userReactions = await message.awaitReactions({
-        filter: (reaction, user) => numberEmojis.concat(allEmoji).includes(reaction.emoji.name ?? "") && user.id === userId,
-        time: 60_000,
-        max: 1
-    });
-
-    return userReactions.first()?.emoji?.name ?? undefined;
-}
-
-function renderCivSelectionMessage(civToBan: Civ, matchedCivs: Civ[]) {
-    return `"${civToBan}" matches ${matchedCivs.length} civs that aren't already banned. Please click one of the reacts to select which one to ban:
-${matchedCivs.map((x, i) => `${numberEmojis[i]}: ${renderCiv(x)}`).join("\n")}
-${allEmoji}: All`
-}
-
 function renderCiv(civ: Civ): string {
     if (!hasLeader(civ)) {
         return civ;
@@ -104,6 +66,3 @@ function renderCiv(civ: Civ): string {
 
     return `${civ.leader} - ${civ.civ}`;
 }
-
-const numberEmojis = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣']
-const allEmoji = '💥'
---
+++ src/Commands/Ban/MultipleChoice.ts
@@ -0,0 +1,51 @@
+import {CommandInteraction, Message} from "discord.js";
+
+const numberEmojis = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣']
+const allEmoji = '💥'
+export const MAX_OPTIONS = numberEmojis.length;
+
+export async function multipleChoice(interaction: CommandInteraction, options: string[], messages: {question: string, timeout: string, selected: (selectedOptions: string[]) => string}): Promise<string[] | undefined>{
+    const reply = await interaction.reply({
+        content: `${messages.question}
+${renderOptionsMessage(options)}`,
+        fetchReply: true
+    });
+    
+    await addReactions(reply, options.length);
+    const reactionEmoji = await waitForUserReaction(reply, interaction.user.id);
+
+    if (reactionEmoji === undefined) {
+        await reply.edit(messages.timeout);
+        return undefined;
+    }
+    if (reactionEmoji === allEmoji) {
+        await reply.edit(messages.selected(options));
+        return options;
+    } else if (numberEmojis.includes(reactionEmoji)) {
+        const selectedOption = options[numberEmojis.indexOf(reactionEmoji)];
+        await reply.edit(messages.selected([selectedOption]));
+        return [selectedOption];
+    }
+}
+
+async function addReactions(message: Message, numChoices: number) {
+    await message.react(allEmoji)
+    for (const emoji of numberEmojis.slice(0, numChoices)) {
+        await message.react(emoji);
+    }
+}
+
+async function waitForUserReaction(message: Message, userId: string) {
+    const userReactions = await message.awaitReactions({
+        filter: (reaction, user) => numberEmojis.concat(allEmoji).includes(reaction.emoji.name ?? "") && user.id === userId,
+        time: 60_000,
+        max: 1
+    });
+
+    return userReactions.first()?.emoji?.name ?? undefined;
+}
+
+function renderOptionsMessage(options: string[]) {
+    return `${options.map((x, i) => `${numberEmojis[i]}: ${x}`).join("\n")}
+${allEmoji}: All`
+}
