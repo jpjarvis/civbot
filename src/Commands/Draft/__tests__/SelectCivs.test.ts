8d85f6d2eda8cb33f6ddff431daa42f175b430d0
Jack Jarvis <jackpjarvis12@gmail.com>
2024-02-17 11:59:34 +0000

    Remove selectCivs tests involving custom civs

--- src/Commands/Draft/__tests__/SelectCivs.test.ts
+++ src/Commands/Draft/__tests__/SelectCivs.test.ts
@@ -1,8 +1,5 @@
-import { Expansion } from "../../../Civs/Expansions";
 import { selectCivs } from "../SelectCivs";
 import { Civs } from "../../../Civs/Civs";
-
-const customCivs = ["custom1"];
 
 describe("selectCivs", () => {
     it("should return no civs if no expansions are given", () => {
@@ -15,19 +12,9 @@ describe("selectCivs", () => {
         expect(civs).toIncludeSameMembers(Civs["civ5-vanilla"]);
     });
 
-    it("should include custom civs from user data if specified", () => {
-        const civs = selectCivs([], []);
-        expect(civs).toIncludeSameMembers(["custom1"]);
-    });
-
     it("should return the civs of multiple expansions when specified", () => {
         const civs = selectCivs(["civ5-vanilla", "lekmod"], []);
         expect(civs).toIncludeSameMembers(Civs["civ5-vanilla"].concat(Civs["lekmod"]));
-    });
-
-    it("should include custom civs with normal expansions when specified", () => {
-        const civs = selectCivs(["civ5-vanilla", "lekmod"], []);
-        expect(civs).toIncludeSameMembers(Civs["civ5-vanilla"].concat(Civs["lekmod"]).concat(customCivs));
     });
 
     it("should not include banned civs", () => {
