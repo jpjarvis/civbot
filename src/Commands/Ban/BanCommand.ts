2a911018e82e4ddf143d75cfe0cccba1cd326f20
Jack Jarvis <jackpjarvis12@gmail.com>
2024-02-17 18:06:45 +0000

    Remove the prompt to select which to ban
    The autocomplete covers this now

--- src/Commands/Ban/BanCommand.ts
+++ src/Commands/Ban/BanCommand.ts
@@ -1,56 +1,38 @@
 import { loadUserData } from "../../UserDataStore";
-import { matchCivs } from "./MatchCivs";
 import { Civ, renderCiv } from "../../Civs/Civs";
 import { ChatInputCommandInteraction } from "discord.js";
-import { MAX_OPTIONS, multipleChoice } from "./MultipleChoice";
-import { banCivs, isBanned } from "./Ban";
+import { banCivs } from "./Ban";
+import { selectCivs } from "../Draft/SelectCivs";
 
 export async function banCommand(interaction: ChatInputCommandInteraction) {
     const serverId = interaction.guildId!;
 
-    const searchString = interaction.options.getString("civ")!;
+    const searchString = interaction.options.getString("civ")!.toLowerCase();
     const userData = await loadUserData(serverId);
+    const userSettings = userData.userSettings[userData.game];
 
-    const allMatchedCivs = matchCivs(searchString, userData.game);
-    const alreadyBannedCivs = allMatchedCivs.filter((x) => isBanned(x, userData));
-    const availableMatchedCivs = allMatchedCivs.filter((x) => !isBanned(x, userData));
+    const allMatchedCivs = selectCivs(
+        userSettings.defaultDraftSettings.expansions ?? [],
+        userSettings.bannedCivs,
+    ).filter((civ) => renderCiv(civ).toLowerCase().includes(searchString));
 
-    if (availableMatchedCivs.length === 0) {
-        if (alreadyBannedCivs.length == 1) {
-            await interaction.reply(`\`${renderCiv(alreadyBannedCivs[0])}\` is already banned.`);
-        } else if (alreadyBannedCivs.length > 1) {
-            await interaction.reply(
-                `"${searchString}" matches ${alreadyBannedCivs.length} civs, but they are all already banned.`,
-            );
-        }
-        await interaction.reply(`No civ called "${searchString}" exists in ${userData.game}.`);
+    if (allMatchedCivs.length == 0) {
+        await interaction.reply(`"${searchString}" didn't match any civs currently included in your draft.`);
         return;
-    } else if (availableMatchedCivs.length > MAX_OPTIONS) {
+    }
+    if (allMatchedCivs.length > 1) {
         await interaction.reply(
-            `"${searchString}" matches ${availableMatchedCivs.length} civs that aren't already banned. Please me more specific.`,
+            `"${searchString}" matches ${allMatchedCivs.length} civs currently included in your draft. Use the autocomplete to find the specific leader/civ you want to ban.`,
         );
         return;
-    } else if (availableMatchedCivs.length > 1) {
-        const chosenCivs = await multipleChoice(interaction, availableMatchedCivs, renderCiv, {
-            question: `"${searchString}" matches ${availableMatchedCivs.length} civs that aren't already banned. Please click one of the reacts to select which one to ban:`,
-            selected: banMessage,
-            timeout: "Request to ban civ timed out.",
-        });
-
-        if (chosenCivs) {
-            await banCivs(serverId, userData, chosenCivs);
-            return;
-        }
+    } else {
+        const chosenCiv = allMatchedCivs[0];
+        await banCivs(serverId, userData, [chosenCiv]);
+        await interaction.reply(banMessage(chosenCiv));
+        return;
     }
-
-    await banCivs(serverId, userData, availableMatchedCivs);
-    await interaction.reply(banMessage(availableMatchedCivs));
 }
 
-function banMessage(bannedCivs: Civ[]) {
-    if (bannedCivs.length == 1) {
-        return `\`${renderCiv(bannedCivs[0])}\` has been banned. It will no longer appear in your drafts.`;
-    }
-
-    return `The following civs have been banned: \`\`\`\n${bannedCivs.map((x) => renderCiv(x)).join("\n")}\`\`\` They will no longer appear in your drafts.`;
+function banMessage(bannedCiv: Civ) {
+    return `\`${renderCiv(bannedCiv)}\` has been banned. It will no longer appear in your drafts.`;
 }
