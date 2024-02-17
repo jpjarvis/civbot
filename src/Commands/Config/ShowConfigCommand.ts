35ab0a7238594a201da2b81cf47ade6c7a116ca2
Jack Jarvis <jackpjarvis12@gmail.com>
2024-02-17 11:20:38 +0000

    Custom civs are always enabled

--- src/Commands/Config/ShowConfigCommand.ts
+++ src/Commands/Config/ShowConfigCommand.ts
@@ -12,7 +12,7 @@ export async function showConfigCommand(interaction: ChatInputCommandInteraction
 
 function renderConfig(userData: UserData): string {
     const activeSettings = userData.userSettings[userData.game];
-    const customCivsEnabled = true;
+    const anyCustomCivs = activeSettings.customCivs.length > 0;
     const anyBannedCivs = activeSettings.bannedCivs.length > 0;
 
     return `
@@ -20,15 +20,13 @@ CivBot is drafting for **${userData.game}**.
 *To change this, use \`/switch-game\`.*
 
 Enabled expansions: \`\`\`${activeSettings.defaultDraftSettings?.expansions?.map(displayName).join("\n")}\`\`\`
-${customCivsEnabled ? customCivsLine(activeSettings.customCivs) : ""}
+${anyCustomCivs ? customCivsLine(activeSettings.customCivs) : ""}
 ${anyBannedCivs ? bannedCivsLine(activeSettings.bannedCivs) : ""}
     `.trim();
 }
 
 function customCivsLine(customCivs: string[]) {
-    if (customCivs.length === 0) {
-        return "No custom civs are defined. Use `/custom-civs` to add some.\n";
-    } else if (customCivs.length > 20) {
+    if (customCivs.length > 20) {
         return `${customCivs.length} custom civs are defined. To see the full list, use \`/custom-civs\`.\n`;
     } else {
         return `Custom civs:\`\`\`${customCivs.sort().join("\n")}\`\`\``;
