4f759a59556a4cb5b036f9d53983a4b72076764a
Jack Jarvis <jackpjarvis12@gmail.com>
2024-12-01 20:20:53 +0000

    Expand the ban tests

--- src/Commands/Ban/__tests__/BanCommand.test.ts
+++ src/Commands/Ban/__tests__/BanCommand.test.ts
@@ -8,40 +8,90 @@ describe("BanCommand", () => {
         await clearTestUserData();
     })
 
-    it("should ban the specified civ", async () => {
-        const interaction = TestInteraction.ban("Russia");
+    describe("Civ 5", () => {
+        it("should ban the specified civ", async () => {
+            const interaction = TestInteraction.ban("Russia");
 
-        await banCommand(interaction.value);
-        
-        const userData = await getTestUserData();
+            await banCommand(interaction.value);
 
-        expect(interaction.output).not.toBeEmpty();
-        expect(userData.userSettings["Civ 5"].bannedCivs).toEqual(["Russia"]);
+            const userData = await getTestUserData();
+
+            expect(interaction.output).not.toBeEmpty();
+            expect(userData.userSettings["Civ 5"].bannedCivs).toEqual(["Russia"]);
+        });
+
+        it("should not ban the civ if the civ is already banned", async () => {
+            await banCommand(TestInteraction.ban("Rome").value);
+
+            const interaction = TestInteraction.ban("Rome");
+            await banCommand(interaction.value);
+
+            const userData = await getTestUserData();
+
+            expect(interaction.output).not.toBeEmpty();
+            expect(userData.userSettings["Civ 5"].bannedCivs).toEqual(["Rome"]);
+        });
+
+        it("should not ban the civ if it doesn't exist", async () => {
+            const interaction = TestInteraction.ban("ThisCivDoesNotExistOhNooooo");
+            await banCommand(interaction.value);
+
+            const userData = await getTestUserData();
+
+            expect(interaction.output).not.toBeEmpty();
+            expect(userData.userSettings["Civ 5"].bannedCivs).toEqual([]);
+        });
     });
 
-    it("should ban based on civ name for Civ 6", async () => {
-        await switchGameCommand(TestInteraction.empty().value);
+    describe("Civ 6", () => {
+        beforeEach(async () => {
+            await switchGameCommand(TestInteraction.empty().value);
+        });
 
-        const interaction = TestInteraction.ban("Russia");
+        it("should ban based on civ name", async () => {
+            const interaction = TestInteraction.ban("Russia");
 
-        await banCommand(interaction.value);
-        
-        const userData = await getTestUserData();
-        
-        expect(interaction.output).not.toBeEmpty();
-        expect(userData.userSettings["Civ 6"].bannedCivs).toEqual([{leader: "Peter", civ: "Russia"}]);
+            await banCommand(interaction.value);
+
+            const userData = await getTestUserData();
+
+            expect(interaction.output).not.toBeEmpty();
+            expect(userData.userSettings["Civ 6"].bannedCivs).toEqual([{leader: "Peter", civ: "Russia"}]);
+        });
+
+        it("should ban based on leader name", async () => {
+            const interaction = TestInteraction.ban("Trajan");
+
+            await banCommand(interaction.value);
+
+            const userData = await getTestUserData();
+
+            expect(interaction.output).not.toBeEmpty();
+            expect(userData.userSettings["Civ 6"].bannedCivs).toEqual([{leader: "Trajan", civ: "Rome"}]);
+        });
+
+        it("should not ban the civ if the civ is already banned", async () => {
+            await banCommand(TestInteraction.ban("Trajan").value);
+
+            const interaction = TestInteraction.ban("Trajan");
+            await banCommand(interaction.value);
+
+            const userData = await getTestUserData();
+
+            expect(interaction.output).not.toBeEmpty();
+            expect(userData.userSettings["Civ 6"].bannedCivs).toEqual([{leader: "Trajan", civ: "Rome"}]);
+        });
+
+        it("should not ban the civ if it doesn't exist", async () => {
+            const interaction = TestInteraction.ban("ThisCivDoesNotExistOhNooooo");
+            await banCommand(interaction.value);
+
+            const userData = await getTestUserData();
+
+            expect(interaction.output).not.toBeEmpty();
+            expect(userData.userSettings["Civ 6"].bannedCivs).toEqual([]);
+        });
     });
 
-    it("should ban based on leader name for Civ 6", async () => {
-        await switchGameCommand(TestInteraction.empty().value);
 
-        const interaction = TestInteraction.ban("Trajan");
-
-        await banCommand(interaction.value);
-
-        const userData = await getTestUserData();
-        
-        expect(interaction.output).not.toBeEmpty();
-        expect(userData.userSettings["Civ 6"].bannedCivs).toEqual([{leader: "Trajan", civ: "Rome"}]);
-    });
 });
